0cd84f80e82bfd26a70ad70289a946c9f8cf2b70
if <ifshuaishuai@gmail.com>
2025-03-29 15:05:54 +0800

    [Add] TestGC 426B

---
+++ Assets/Scripts/TestGC.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.Localization.SmartFormat;
+
+public class TestGC : MonoBehaviour
+{
+    private CultureInfo _cultureEnglish = CultureInfo.GetCultureInfo("en");
+
+    private void Update()
+    {
+        int number = 2;
+        
+        // 426 B
+        string result = Smart.Format(_cultureEnglish, "There {0:plural:is 1 item|are {} items}.", number);
+    }
+}
