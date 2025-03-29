c06c33e6fe56ad56af7415df65fda0f59c057511
if <ifshuaishuai@gmail.com>
2025-03-29 17:13:59 +0800

    [Add] Template Format T1 to T8

--- Assets/Pluralization/Smart Format/Smart.cs
+++ Assets/Pluralization/Smart Format/Smart.cs
@@ -9,7 +9,15 @@ namespace UnityEngine.Localization.SmartFormat
     /// </summary>
     public static class Smart
     {
-        private static object[] s_ArrayCache0 = new object[1];
+        private static object[] s_ArrayCache1 = new object[1];
+        private static object[] s_ArrayCache2 = new object[2];
+        private static object[] s_ArrayCache3 = new object[3];
+        private static object[] s_ArrayCache4 = new object[4];
+        private static object[] s_ArrayCache5 = new object[5];
+        private static object[] s_ArrayCache6 = new object[6];
+        private static object[] s_ArrayCache7 = new object[7];
+        private static object[] s_ArrayCache8 = new object[8];
+        
             
         /// <inheritdoc cref="SmartFormatter.Format(string, object[])"/>
         public static string Format(string format, params object[] args)
@@ -18,11 +26,105 @@ namespace UnityEngine.Localization.SmartFormat
         }
 
         /// <inheritdoc cref="SmartFormatter.Format(IFormatProvider, string, object[])"/>
-        public static string Format<T0>(IFormatProvider provider, string format, T0 arg0)
+        public static string Format<T1>(IFormatProvider provider, string format, T1 arg1)
         {
-            s_ArrayCache0[0] = arg0;
+            s_ArrayCache1[0] = arg1;
+            string result = Default.Format(provider, format, s_ArrayCache1);
             
-            return Default.Format(provider, format, s_ArrayCache0);
+            Array.Clear(s_ArrayCache1, 0, s_ArrayCache1.Length);
+            return result;
+        }
+        
+        public static string Format<T1, T2>(IFormatProvider provider, string format, T1 arg1, T2 arg2)
+        {
+            s_ArrayCache2[0] = arg1;
+            s_ArrayCache2[1] = arg2;
+            string result = Default.Format(provider, format, s_ArrayCache2);
+            
+            Array.Clear(s_ArrayCache2, 0, s_ArrayCache2.Length);
+            return result;
+        }
+        
+        
+        public static string Format<T1, T2, T3>(IFormatProvider provider, string format, T1 arg1, T2 arg2, T3 arg3)
+        {
+            s_ArrayCache3[0] = arg1;
+            s_ArrayCache3[1] = arg2;
+            s_ArrayCache3[2] = arg3;
+            string result = Default.Format(provider, format, s_ArrayCache3);
+            
+            Array.Clear(s_ArrayCache3, 0, s_ArrayCache3.Length);
+            return result;
+        }
+        
+        public static string Format<T1, T2, T3, T4>(IFormatProvider provider, string format, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            s_ArrayCache4[0] = arg1;
+            s_ArrayCache4[1] = arg2;
+            s_ArrayCache4[2] = arg3;
+            s_ArrayCache4[3] = arg4;
+            string result = Default.Format(provider, format, s_ArrayCache4);
+            
+            Array.Clear(s_ArrayCache4, 0, s_ArrayCache4.Length);
+            return result;
+        }
+        
+        public static string Format<T1, T2, T3, T4, T5>(IFormatProvider provider, string format, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        {
+            s_ArrayCache5[0] = arg1;
+            s_ArrayCache5[1] = arg2;
+            s_ArrayCache5[2] = arg3;
+            s_ArrayCache5[3] = arg4;
+            s_ArrayCache5[4] = arg5;
+            string result = Default.Format(provider, format, s_ArrayCache5);
+            
+            Array.Clear(s_ArrayCache5, 0, s_ArrayCache5.Length);
+            return result;
+        }
+        
+        public static string Format<T1, T2, T3, T4, T5, T6>(IFormatProvider provider, string format, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6)
+        {
+            s_ArrayCache6[0] = arg1;
+            s_ArrayCache6[1] = arg2;
+            s_ArrayCache6[2] = arg3;
+            s_ArrayCache6[3] = arg4;
+            s_ArrayCache6[4] = arg5;
+            s_ArrayCache6[5] = arg6;
+            string result = Default.Format(provider, format, s_ArrayCache6);
+            
+            Array.Clear(s_ArrayCache6, 0, s_ArrayCache6.Length);
+            return result;
+        }
+        
+        public static string Format<T1, T2, T3, T4, T5, T6, T7>(IFormatProvider provider, string format, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7)
+        {
+            s_ArrayCache7[0] = arg1;
+            s_ArrayCache7[1] = arg2;
+            s_ArrayCache7[2] = arg3;
+            s_ArrayCache7[3] = arg4;
+            s_ArrayCache7[4] = arg5;
+            s_ArrayCache7[5] = arg6;
+            s_ArrayCache7[6] = arg7;
+            string result = Default.Format(provider, format, s_ArrayCache7);
+            
+            Array.Clear(s_ArrayCache7, 0, s_ArrayCache7.Length);
+            return result;
+        }
+        
+        public static string Format<T1, T2, T3, T4, T5, T6, T7, T8>(IFormatProvider provider, string format, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8)
+        {
+            s_ArrayCache8[0] = arg1;
+            s_ArrayCache8[1] = arg2;
+            s_ArrayCache8[2] = arg3;
+            s_ArrayCache8[3] = arg4;
+            s_ArrayCache8[4] = arg5;
+            s_ArrayCache8[5] = arg6;
+            s_ArrayCache8[6] = arg7;
+            s_ArrayCache8[7] = arg8;
+            string result = Default.Format(provider, format, s_ArrayCache8);
+            
+            Array.Clear(s_ArrayCache8, 0, s_ArrayCache8.Length);
+            return result;
         }
 
         /// <inheritdoc cref="Format(string, object[])"/>
